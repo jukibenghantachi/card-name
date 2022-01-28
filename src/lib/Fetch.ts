286faf755af0f47335f7f8901c4b050fd176676f
Juki <jukibenghantachi@gmail.com>
2022-01-28 18:40:59 +0700

    fix(prettier): change tab width

--- src/lib/Fetch.ts
+++ src/lib/Fetch.ts
@@ -1,26 +1,26 @@
-import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch'
 
 export class Fetch {
-	/**
-	 * Fetch username github
-	 * @param username username github
-	 * @returns json
-	 * @deprecated due github ratelimit issue
-	 */
-	public async fetchUser(username: string): Promise<string> {
-		const res = await fetch(`https://api.github.com/users/${username}`);
-		const stringify = JSON.stringify(await res.json());
-		return stringify;
-	}
+    /**
+     * Fetch username github
+     * @param username username github
+     * @returns json
+     * @deprecated due github ratelimit issue
+     */
+    public async fetchUser(username: string): Promise<string> {
+        const res = await fetch(`https://api.github.com/users/${username}`)
+        const stringify = JSON.stringify(await res.json())
+        return stringify
+    }
 
-	/**
-	 * Fetch image and convert to base64
-	 * @param url url image
-	 * @returns base64 string
-	 */
-	public async fetchImage(url: string): Promise<string> {
-		const res = await fetch(url);
-		const buffer = await res.arrayBuffer();
-		return `data:image/png;base64,${Buffer.from(buffer).toString('base64')}`;
-	}
+    /**
+     * Fetch image and convert to base64
+     * @param url url image
+     * @returns base64 string
+     */
+    public async fetchImage(url: string): Promise<string> {
+        const res = await fetch(url)
+        const buffer = await res.arrayBuffer()
+        return `data:image/png;base64,${Buffer.from(buffer).toString('base64')}`
+    }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,13 @@
 export interface CardData {
-	name: string | string[];
-	bio: string | string[];
-	facebook: string | string[];
-	youtube: string | string[];
-	twitter: string | string[];
-	instagram: string | string[];
-	github: string | string[];
-	discord: string | string[];
-	telegram: string | string[];
-	linkedin: string | string[];
-	image: string | string[];
+    name: string | string[]
+    bio: string | string[]
+    facebook: string | string[]
+    youtube: string | string[]
+    twitter: string | string[]
+    instagram: string | string[]
+    github: string | string[]
+    discord: string | string[]
+    telegram: string | string[]
+    linkedin: string | string[]
+    image: string | string[]
 }
